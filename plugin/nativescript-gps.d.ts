710c6d78d3a71f41b145f67081ddbde4b245b0aa
Martin Guillon <martin.guillon@akylas.fr>
2019-05-10 17:01:14 +0200

    cleanup

--- plugin/nativescript-gps.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * iOS and Android apis should match.
- * It doesn't matter if you export `.ios` or `.android`, either one but only one.
- */
-export * from './nativescript-gps.ios';
-
-// Export any shared classes, constants, etc.
-export * from './nativescript-gps.common';
